42d536b8ea0004c7e2b68132255fcbc7cd9bf656
Kumorai9-star <kumorai9@gmail.com>
2026-07-08 07:42:19 +0545

    Debug Mongo URI

--- config/db.js
+++ config/db.js
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI =", JSON.stringify(process.env.MONGO_URI));
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("Database Connection Error:", error.message);
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
+
 
 export default connectDB;
